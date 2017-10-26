be8d1c1fdf9c57f6e7c1e0762e42a6d97dde2e16
BillaMilla646 <bamiller6649@gmail.com>
2017-10-26 12:39:52 -0400

    updates

--- MillerJohnsonp3.js
+++ MillerJohnsonp3.js
@@ -6,17 +6,13 @@
 
 //needed libraries
 var http = require("http"),
-	   require('url');
+    url =  require('url');
 
 var STARTPORT = 2000;
 var ENDPORT = 30000;
 var HOSTNAME = 'iris.cs.uky.edu';
 //rate of advertisements as a percentage, ints only
 var ADVERT_RATE = 33;
-//MP3 filenames
-var mp3s = [];
-//JPG filenames
-var jpgs = [];
 
 
 
@@ -39,20 +35,32 @@ function giveFile(filename) {
 
 //Validates the URL.
 //Returns false if name is not valid.
-//Returns true if name is valid, but isn't recognized.
-//Returns a string with the filename if the name is valid and recognized.
+//Returns a string with the filename if name is valid.
 function validateURL(fullURL) {
 	var filename = fullURL.substring(fullURL.lastIndexOf('/'), fullURL.length);
-	allFiles = mp3s.concat(jpgs);	
+	var regx1 = new RegExp("[a-zA-Z0-9_]*.mp3");
+	var regx2 = new RegExp("[a-zA-Z0-9_]*.jpg");
+	if(regx1.test(filename)||regx2.test(filename)) {
+		return filename;
+	}
+	return false;
 }
 
 function start() {
 	//based on/borrowed from:
 	//https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
 	var port = Math.floor(Math.random()*ENDPORT-STARTPORT+1)+STARTPORT;
-	http.createServer(function(request, response) {
+	var server = http.createServer(function(request, response) {
 		var givenURL = request.url;
-		
-	}
+		response.statusCode = 200;
+		response.setHeader('Content-Type', 'text/plain');
+		response.end('Hello, World!  You requested the following URL: '+givenURL+'\n');
+	});
+
+	server.listen(port, HOSTNAME, function() {
+		console.log('Server running at http://'+ HOSTNAME +':'+ port +'/');
+		console.log('Hello, World!');
+	});
 	
 }
+start();
