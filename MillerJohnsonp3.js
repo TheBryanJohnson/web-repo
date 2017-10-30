4c915f033494ca0ff99f03eec1569faf06590bcb
BillaMilla646 <bamiller6649@gmail.com>
2017-10-30 12:01:01 -0400

    update, problems still exist

--- MillerJohnsonp3.js
+++ MillerJohnsonp3.js
@@ -7,10 +7,11 @@
 //needed libraries
 var http = require("http"),
     url =  require('url');
+const fs = require('fs');
 
-var STARTPORT = 2000;
-var ENDPORT = 30000;
-var HOSTNAME = 'iris.cs.uky.edu';
+var STARTPORT = 6660;
+var ENDPORT = 6660;
+var HOSTNAME = 'iris.cs.engr.uky.edu';
 //rate of advertisements as a percentage, ints only
 var ADVERT_RATE = 33;
 
@@ -29,37 +30,70 @@ function giveAdvert() {
 	return false;
 }
 
-function giveFile(filename) {
-	
+function giveFile(filename, response) {
+	if(fs.existsSync('./'+filename)) {
+		fs.readFile('./'+filename, (err, data) => {
+			if (err) {
+				response.statusCode = 403;
+				response.setHeader('Content-Type', 'text/html');
+				response.write("<h3>File not found!</h3>");
+			}
+			else {
+				//serve file
+				fs.open('./'+filename, 'r', (err, fd) => {
+					if (err) {
+						console.log("problem in serving");
+					}
+				});
+			}
+		});
+	}
+	else {
+		response.write("<h3>File doesn't exist!</h3>");
+	}
 }
 
 //Validates the URL.
 //Returns false if name is not valid.
 //Returns a string with the filename if name is valid.
-function validateURL(fullURL) {
-	var filename = fullURL.substring(fullURL.lastIndexOf('/'), fullURL.length);
-	var regx1 = new RegExp("[a-zA-Z0-9_]*.mp3");
-	var regx2 = new RegExp("[a-zA-Z0-9_]*.jpg");
-	if(regx1.test(filename)||regx2.test(filename)) {
-		return filename;
+function validateFilename(fileName) {
+	var regx1 = new RegExp("[0-9a-zA-z_]+.mp3$");
+	var regx2 = new RegExp("[0-9a-zA-z_]+.jpg$");
+	console.log("a request for ["+fileName+"] was made");
+	if(regx1.test(fileName)||regx2.test(fileName)) {
+		return fileName;
 	}
+	//console.log(fileName+" comes up as false");
 	return false;
 }
 
 function start() {
 	//based on/borrowed from:
 	//https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-	var port = Math.floor(Math.random()*ENDPORT-STARTPORT+1)+STARTPORT;
+	var port;
+	if(STARTPORT == ENDPORT) port = STARTPORT;
+	else port = Math.floor(Math.random()*ENDPORT-STARTPORT+1)+STARTPORT;
 	var server = http.createServer(function(request, response) {
 		var givenURL = request.url;
-		response.statusCode = 200;
-		response.setHeader('Content-Type', 'text/plain');
-		response.end('Hello, World!  You requested the following URL: '+givenURL+'\n');
+		//substring out the /
+		givenURL = givenURL.substring(1, givenURL.length);
+		//response.setHeader('Content-Type', 'text/html;');
+		fileName = validateFilename(givenURL);
+		if(fileName === false) {
+			response.statusCode = 403;
+			response.setHeader('Content-Type', 'text/html;');
+			response.write("<h3>Invalid file name!</h3>");
+		}
+		else {
+			giveFile(fileName, response);
+		}
+		//temporary
+		//console.log("A request for ["+fileName+"] was made.");
+		//response.end('You requested the following file: '+fileName+'\n');
 	});
 
 	server.listen(port, HOSTNAME, function() {
 		console.log('Server running at http://'+ HOSTNAME +':'+ port +'/');
-		console.log('Hello, World!');
 	});
 	
 }
