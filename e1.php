16a699e3aa3c5d943b2360431b89e0791c4b8aa1
BillaMilla646 <bamiller6649@gmail.com>
2017-11-16 13:34:15 -0500

    fanxelk! yay

---
+++ e1.php
@@ -0,0 +1,76 @@
+<?php
+
+/*Authors: William Miller, Bryan Johnson
+* 
+*
+*
+*
+*
+*
+*/
+
+
+function end_html() {
+
+echo "
+</body>
+</html>
+";
+}
+
+if (isset($_GET['title']) && isset($_GET['results'])) {
+	//validate title and results
+	process_form();
+} else {
+	display_form();
+}
+
+
+function process_form() {
+
+start_html();
+
+$title = $_GET['title'];
+$results = $_GET['results'];
+$searchTerm = $_GET['searchTerm'];
+
+echo $title, $results, $searchTerm;
+
+end_html();
+
+
+}
+
+function display_form() {
+
+start_html();
+?>
+<form action="e1.php" method="get">
+	Title:
+	<input type='number' name='title'><br><br>
+	Results:
+	<input type='number' name='results'><br><br>
+	Search Term:
+	<input type='number' name='searchTerm'><br><br>
+	<input type='submit' value='Pls'>
+</form>
+
+<?php
+end_html();
+}
+
+function start_html() {
+
+echo "
+<html>
+<head>
+<title>Sports!</title>
+</head>
+<body>
+<h1>FanXelk</h1>
+";
+
+}
+
+
+?>
